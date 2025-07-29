f7fa0a89e5bf7886ff8d4329c1689cdcb20742e5
FemboyKitten <boyinaskirt187@gmail.com>
2025-07-29 09:48:35 +0200

    ipgeolocator angepasst

--- ipgeolocator/ipgeolocator.js
+++ ipgeolocator/ipgeolocator.js
@@ -21,3 +21,31 @@ function geolocate(){
         }
     });
 }
+function geolocateOwnIP(){
+    document.getElementById("search-own-ip-btn").addEventListener("click", async function() {
+        try {
+            const ip = await getIp();
+
+            if (ip) {
+                fetch(`http://ip-api.com/json/${ip}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.status === 'success') {
+                            alert(`Your IP Address: ${data.query}\nCountry: ${data.country}\nRegion: ${data.regionName}\nCity: ${data.city}\nISP: ${data.isp}`);
+                        } else {
+                            alert('Error retrieving data for your IP address.');
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                        alert('An error occurred while fetching your IP address information.');
+                    });
+            } else {
+                alert('Could not retrieve your IP address.');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred while fetching your IP address.');
+        }
+    });
+}
