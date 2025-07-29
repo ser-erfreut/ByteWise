33dfd9d15db1629808c02ba343ef820f989201e2
FemboyKitten <boyinaskirt187@gmail.com>
2025-07-29 09:09:08 +0200

    ipgeolocator angefnagen

--- ipgeolocator/ipgeolocator.js
+++ ipgeolocator/ipgeolocator.js
@@ -1,24 +1,23 @@
 function geolocate(){
-    if (ipaddress) {
-
-        document.getElementById("search-btn").addEventListener("click", function() {
-            var inputValueIP  = document.getElementById("InputIP").value;
-        });
-
-        fetch(`http://ip-api.com/json/${ipaddress}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.status === 'success') {
-                    alert(`IP Address: ${data.query}\nCountry: ${data.country}\nRegion: ${data.regionName}\nCity: ${data.city}\nISP: ${data.isp}`);
-                } else {
-                    alert('Error retrieving data for the provided IP address.');
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('An error occurred while fetching the IP address information.');
-            });
-    } else {
-        alert('No IP address entered.');
-    }
+    document.getElementById("search-btn").addEventListener("click", function() {
+        var inputValueIP = document.getElementById("InputIP").value;
+        if (inputValueIP) {
+            
+            fetch(`http://ip-api.com/json/${inputValueIP}`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status === 'success') {
+                        alert(`IP Address: ${data.query}\nCountry: ${data.country}\nRegion: ${data.regionName}\nCity: ${data.city}\nISP: ${data.isp}`);
+                    } else {
+                        alert('Error retrieving data for the provided IP address.');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('An error occurred while fetching the IP address information.');
+                });
+        } else {
+            alert('No IP address entered.');
+        }
+    });
 }
